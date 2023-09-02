fdee10373ac8008e57e97495fbf240326c66a159
U200709069 <berkebjk16@gmail.com>
2023-09-02 15:54:50 +0300

    Category product eklendi

---
+++ IdentityCatalog/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace IdentityCatalog.Models
+{
+    public class Category
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted {  get; set; }
+    }
+}
---
+++ IdentityCatalog/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace IdentityCatalog.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageURL { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Currency { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
