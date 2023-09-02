61fb908359cfc64553088372ff3b1416df636ec2
U200709069 <berkebjk16@gmail.com>
2023-09-02 16:53:14 +0300

    category repo oluşturuldu, getAllCategories oluşturuldu

---
+++ IdentityCatalog/Repositories/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿
+using IdentityCatalog.Models;
+
+namespace IdentityCatalog.Repositories
+{
+    public class CategoryRepository
+    {
+
+        private readonly ApplicationDbContext context;
+        public CategoryRepository() {
+            this.context = new ApplicationDbContext();
+        }
+
+        public List<Category> AllCategories()
+        {
+            var categories = context.Categories.ToList();
+            return categories;
+
+        }
+    }
+}
