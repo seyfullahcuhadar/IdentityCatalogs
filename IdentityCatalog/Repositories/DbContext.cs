866b1dc7d3008c136850844cb4d9e06b3084ad5f
Seyfullah Çuhadar <78440712+seyfullahcuhadar@users.noreply.github.com>
2023-09-02 15:30:15 +0300

    Application db context hata giderildi

--- IdentityCatalog/Repositories/DbContext.cs
+++ IdentityCatalog/Repositories/DbContext.cs
@@ -2,7 +2,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace IdentityCatalog.Repositories;
 
-public class ApplicationDbContext:DbContext
+public class ApplicationDbContext : DbContext
 {
-    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+    {
+        
+    }
 }
